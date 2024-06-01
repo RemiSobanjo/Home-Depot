6abcba964dd2b3e628d5bc58264c595c55dfc70b
RemiSobanjo <53947149+RemiSobanjo@users.noreply.github.com>
2024-06-01 17:25:26 +0100

    mail feature created

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const contactModel = require("../models/contacts.js")
+const contactModel = require("../models/contacts.js");
+const {welcomeUser} = require("../utils/emails/contact.js");
 
 router.post("/enquiry", async(req, res) => {
     const {first_name, last_name, email, phone_number, subject, message} = req.body
@@ -16,6 +17,10 @@ router.post("/enquiry", async(req, res) => {
     }
    // const saveMsg = await contactModel.create(contactObj);
 
+   //send mail message
+   await welcomeUser(email, first_name, subject)
+
+   
     return res.status(201).json({
         msg: "Thank you for contacting us, we will get back to you shortly.",
         //data:  saveMsg
---
+++ utils/emails/contact.js
@@ -0,0 +1,45 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+exports.welcomeUser = async(email, first_name, subject) => {
+    const salutation = function(){
+        if(first_name){
+            return `Dear ${first_name}`
+        }
+        else{
+            return "Hi"
+        }
+    }
+
+    console.log(salutation());
+    try{
+        var smtpConfig = {
+            service: process.env.SERVICE,
+            host: process.env.HOST,
+            port: 465,
+            secure: true, // use SSL
+            auth: {
+                user: process.env.MAILER,
+                pass: process.env.PASSMAILER,
+            },
+            tls : { rejectUnauthorized: false }
+        };
+
+        const transporter = nodemailer.createTransport(smtpConfig); 
+
+        await transporter.sendMail({
+            from: process.env.MAILER,
+            to: email,
+            subject: subject,
+            html: `<b> ${salutation()}</b></br>
+            <p>
+                We received your mail. We are working on it and 
+                <br /> 
+                will contact you as soon as possible
+            </p>`
+        });
+        console.log("email sent successfully");
+    } catch(error){
+        console.log(error, "email not sent");
+    }
+};
